3678c5ae46f2b48d051a301d74f21a651b13c485
Harshit <harshit@moneyloji.com>
2023-04-12 17:56:16 +0530

    hy

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Porfolio Shailendra
-        </a>
-      </header>
+      <h1>hyy</h1>
     </div>
   );
 }
